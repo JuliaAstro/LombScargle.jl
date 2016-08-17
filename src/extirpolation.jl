8f5bd225cf58fed07e1d1b85018b2016995a4699
Mosè Giordano <mose@gnu.org>
2016-08-17 16:01:43 +0200

    A few fixes to trig_sum-related functions

--- src/extirpolation.jl
+++ src/extirpolation.jl
@@ -11,32 +11,32 @@
 #
 ### Code:
 
-function add_at!{R1<:Real,R2<:Real}(arr::AbstractVector{R1},
-                                    ind::AbstractVector{R2},
-                                    val::Real)
+function add_at!{N1<:Number,N2<:Number}(arr::AbstractVector{N1},
+                                        ind::AbstractVector{N2},
+                                        val::Real)
     for i in ind
         arr[mod(i, length(arr)) + 1] += val
     end
 end
 
-function add_at!{R1<:Real,R2<:Real,R3<:Real}(arr::AbstractVector{R1},
-                                             ind::AbstractVector{R2},
-                                             vals::AbstractVector{R3})
+function add_at!{N1<:Number,N2<:Number,N3<:Number}(arr::AbstractVector{N1},
+                                                   ind::AbstractVector{N2},
+                                                   vals::AbstractVector{N3})
     for i in eachindex(ind)
         arr[mod(ind[i], length(arr)) + 1] += vals[i]
     end
 end
 
-function extirpolate{R1<:Real,R2<:Real}(X::AbstractVector{R1},
-                                        Y::AbstractVector{R2},
-                                        N::Integer=0, M::Integer=4)
+function extirpolate{RE<:Real,NU<:Number}(X::AbstractVector{RE},
+                                          Y::AbstractVector{NU},
+                                          N::Integer=0, M::Integer=4)
     @assert length(X) == length(Y)
     x, y = collect(X), collect(Y)
     if N <= 0
         # Get the maximum of "X", `maximum' has a faster method for ranges.
         N = trunc(Int, maximum(X) + 0.5*M + 1)
     end
-    result = zeros(R2, N)
+    result = zeros(NU, N)
     integers = find(isinteger, x)
     add_at!(result, trunc(Int, x[integers]), y[integers])
     deleteat!(x, integers)
@@ -66,14 +66,14 @@ function trig_sum{R1<:Real,R2<:Real}(t::AbstractVector{R1},
     Nfft = nextpow2(N * oversampling)
     t0 = minimum(t)
     if f0 > 0
-        h = h * exp(2im * pi * f0 * (t - t0))
+        h = h .* exp(2im * pi * f0 * (t - t0))
     end
     tnorm = mod(((t - t0) * Nfft * df), Nfft)
     grid = extirpolate(tnorm, h, Nfft, Mfft)
     fftgrid = Nfft * ifft(grid)[1:N]
     if t0 != 0
-        f = f0 + df * 0:(N - 1)
-        fftgrid *= exp(2im * pi * t0 * f)
+        f = f0 + df * (0:N - 1)
+        fftgrid .*= exp(2im * pi * t0 * f)
     end
     S = real(fftgrid)
     C = imag(fftgrid)
