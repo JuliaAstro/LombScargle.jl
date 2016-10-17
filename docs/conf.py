3cd7db5b2be562f3d571bc2ddd235e90ebea8f02
Mosè Giordano <mose@gnu.org>
2016-10-17 20:38:21 +0200

    Release v0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ copyright = '2016, ' + author
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.1'
+version = '0.1.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
