eacb295dd7100ed9474966ca9cb3356d4a159606
Mosè Giordano <mose@gnu.org>
2017-01-18 01:53:34 +0100

    One more dot

--- src/extirpolation.jl
+++ src/extirpolation.jl
@@ -52,7 +52,7 @@ function extirpolate!{RE<:Real,NU<:Number}(result,
             denominator *= j/(j - M)
         end
         ind = ilo + (M - j)
-        add_at!(result, ind, numerator ./ (denominator * (x .- ind .+ 1)))
+        add_at!(result, ind, numerator ./ (denominator .* (x .- ind .+ 1)))
     end
     return result
 end
