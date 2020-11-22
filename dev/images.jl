0d384ca823f3ce6db9bf020a81b497f564212645
Documenter.jl <documenter@juliadocs.github.io>
2020-11-22 17:14:57 +0000

    build based on 1167abd

--- dev/images.jl
+++ dev/images.jl
@@ -17,7 +17,9 @@
 #
 ### Code:
 
-srand(42)
+using StableRNGs
+
+rng = StableRNG(42)
 
 using Measurements, LombScargle, Plots
 
@@ -26,25 +28,25 @@ sz = (1200, 800)
 # First periodograms
 ntimes = 1001
 t = range(0.01, stop=10pi, length=ntimes)
-t += step(t)*rand(ntimes)
-s = sinpi(t) + 1.5cospi(2t) + rand(ntimes)
+t += step(t)*rand(rng, ntimes)
+s = @. sinpi(t) + 1.5cospi(2t) + rand(rng)
 p = lombscargle(t, s)
 plot(freqpower(p)..., size = sz, xlim = (0.0, 2.0), xlabel = "Frequency",
      ylabel = "Lomb–Scargle power")
-savefig("freq-periodogram.png")
+savefig(joinpath(@__DIR__, "freq-periodogram.png"))
 plot(periodpower(p)..., size = sz, xlim = (0.5, 2.5), xlabel = "Period",
      ylabel = "Lomb–Scargle power")
-savefig("period-periodogram.png")
+savefig(joinpath(@__DIR__, "period-periodogram.png"))
 
 # signal with uncertainties
 ntimes = 1001
 t = range(0.01, stop=10pi, length=ntimes)
-s = sinpi(2t)
-errors = rand(0.1:1e-3:4.0, ntimes)
+s = @. sinpi(2t)
+errors = rand(rng, 0.1:1e-3:4.0, ntimes)
 p = lombscargle(t, s, errors, maximum_frequency=1.5)
 plot(freqpower(p)..., size = sz, xlim = (0.25, 1.5), xlabel = "Frequency",
      ylabel = "Lomb–Scargle power")
-savefig("freq-uncertainties.png")
+savefig(joinpath(@__DIR__, "freq-uncertainties.png"))
 plot(periodpower(p)..., size = sz, xlim = (0.75, 2.0), xlabel = "Period",
      ylabel = "Lomb–Scargle power")
-savefig("period-uncertainties.png")
+savefig(joinpath(@__DIR__, "period-uncertainties.png"))
